e00d21e349af20a20316dc4d7189acd1296b7e1a
jjm2317 <wlaks2317@gmail.com>
2021-01-03 23:18:27 +0900

    feat: Review Redux

--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './exercise';
+import './exercise'
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
